ceb613b509eebda6744654792ed593b5a6d99ad7
modotte <runecroon@gmail.com>
2021-12-15 23:35:27 +0800

    Make swat dir dynamic

--- Launcher.fs
+++ Launcher.fs
@@ -18,34 +18,32 @@ module Client =
         PreModFolder: string
     }
 
-    let [<Literal>] SWAT_INSTALLATION_DIRECTORY = @"C:\GOG Games\SWAT 4"
-
     let private asArchiveFile gameMod =
         let modArchiveName = $"{gameMod.Maintainer}-{gameMod.Version}-{gameMod.Name}"
         match gameMod.Archive with
         | Zip -> modArchiveName + ".zip"
         | Rar -> modArchiveName + ".rar"
 
-    let downloadMod gameMod = 
+    let downloadMod gameMod swatDir = 
         // TODO: Check on installed mod dir instead
-        if File.Exists(SWAT_INSTALLATION_DIRECTORY + (asArchiveFile gameMod)) then
+        if File.Exists(swatDir + (asArchiveFile gameMod)) then
             Error $"{gameMod.Name} already exist!"
         else
             let a = (asArchiveFile gameMod)
-            WebClient().DownloadFile(gameMod.Url, Path.Combine(SWAT_INSTALLATION_DIRECTORY, a))
+            WebClient().DownloadFile(gameMod.Url, Path.Combine(swatDir, a))
             Ok $"{a} downloaded"
 
-    let extractArchive gameMod =
+    let extractArchive gameMod swatDir =
         let modDirName = $"{gameMod.Maintainer}-{gameMod.Version}-{gameMod.Name}"
-        let archivePath = Path.Combine(SWAT_INSTALLATION_DIRECTORY, (asArchiveFile gameMod))
-        Compression.ZipFile.ExtractToDirectory(archivePath, SWAT_INSTALLATION_DIRECTORY)
+        let archivePath = Path.Combine(swatDir, (asArchiveFile gameMod))
+        Compression.ZipFile.ExtractToDirectory(archivePath, swatDir)
         Directory.Move(
-            Path.Combine(SWAT_INSTALLATION_DIRECTORY, gameMod.PreModFolder),
-            Path.Combine(SWAT_INSTALLATION_DIRECTORY, modDirName))
+            Path.Combine(swatDir, gameMod.PreModFolder),
+            Path.Combine(swatDir, modDirName))
 
-    let launchMod gameMod =
+    let launchMod gameMod swatDir =
         let modDirName = $"{gameMod.Maintainer}-{gameMod.Version}-{gameMod.Name}"
-        let modDir = Path.Combine(SWAT_INSTALLATION_DIRECTORY, modDirName)
+        let modDir = Path.Combine(swatDir, modDirName)
         let systemDir = Path.Combine(modDir, "System")
         
         Directory.SetCurrentDirectory(systemDir)
@@ -65,8 +63,8 @@ module Launcher =
     open Avalonia.FuncUI.DSL
     open Avalonia.Layout
     
-    type Model = { Status: string; IsModRunning: bool }
-    let init = { Status = ""; IsModRunning = false }
+    type Model = { SwatInstallationDirectory: string; Status: string; IsModRunning: bool }
+    let init = { SwatInstallationDirectory = @"C:\GOG Games\SWAT 4"; Status = ""; IsModRunning = false }
 
     type Msg = Install | Uninstall | Launch
     let update (msg: Msg) (model: Model) : Model =
@@ -83,15 +81,15 @@ module Launcher =
         match msg with
         | Install ->
             printfn "Download started.."
-            match Client.downloadMod gameMod with
+            match Client.downloadMod gameMod model.SwatInstallationDirectory with
             | Error err -> { model with Status = err }
             | Ok m -> 
-                Client.extractArchive gameMod
+                Client.extractArchive gameMod model.SwatInstallationDirectory
                 printfn "Extraction started.."
                 { model with Status = m}
         | Uninstall -> { model with Status = "Mod uninstalled" }
         | Launch ->
-            Client.launchMod gameMod |> ignore
+            Client.launchMod gameMod model.SwatInstallationDirectory |> ignore
             { model with Status = gameMod.Name + " has been launched"; IsModRunning = true }
     
     let view (model: Model) (dispatch) =
@@ -99,6 +97,7 @@ module Launcher =
             DockPanel.children [
                 Button.create [
                     Button.dock Dock.Bottom
+                    // FIXME: Find a way to emit this state change.
                     Button.isEnabled (not model.IsModRunning)
                     Button.onClick (fun _ -> dispatch Launch)
                     Button.content "Launch Mod"
@@ -113,12 +112,9 @@ module Launcher =
                     Button.onClick (fun _ -> dispatch Install)
                     Button.content "Install"
                 ]
-                TextBlock.create [
-                    TextBlock.dock Dock.Top
-                    TextBlock.fontSize 32.0
-                    TextBlock.verticalAlignment VerticalAlignment.Center
-                    TextBlock.horizontalAlignment HorizontalAlignment.Center
-                    TextBlock.text (string model.Status)
+                TextBox.create [
+                    TextBox.dock Dock.Bottom
+                    TextBox.text model.SwatInstallationDirectory
                 ]
             ] 
         ]       
