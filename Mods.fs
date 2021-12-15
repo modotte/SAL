6f36f5b58a71145f12eb45fec7c62516a79e75e8
modotte <runecroon@gmail.com>
2021-12-16 04:38:21 +0800

    Clean

--- Mods.fs
+++ Mods.fs
@@ -2,4 +2,15 @@ namespace SAL.Data
 
 // TODO: Move this into mods.json.
 module Mods =
-    let mods = []
+    open SAL.DomainModel
+
+    let mods: Mod array = [|
+        {
+            Mod.Name = "SEF"
+            Mod.Maintainer = "eezstreet"
+            Mod.Version = "v7.0"
+            Mod.Url = "https://www.moddb.com/downloads/mirror/195627/115/35d7c155b0249f6ca4aae6fb2a366cda/?referer=https%3A%2F%2Fwww.moddb.com%2Fmods%2Fswat-elite-force%2Fdownloads"
+            Mod.Origin = OriginType.Official
+            Mod.PreExtractFolder = "SEF"
+        }
+    |]
