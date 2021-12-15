dcc5aeb5af8c74ffc1b3814481505c16c152d584
modotte <runecroon@gmail.com>
2021-12-15 16:18:27 +0800

    Fix boilerplate

--- Launcher.fs
+++ Launcher.fs
@@ -5,41 +5,35 @@ module Launcher =
     open Avalonia.FuncUI.DSL
     open Avalonia.Layout
     
-    type State = { count : int }
-    let init = { count = 0 }
+    type Model = { Status: string }
+    let init = { Status = "" }
 
-    type Msg = Increment | Decrement | Reset
+    type Msg = Install | Uninstall
 
-    let update (msg: Msg) (state: State) : State =
+    let update (msg: Msg) (model: Model) : Model =
         match msg with
-        | Increment -> { state with count = state.count + 1 }
-        | Decrement -> { state with count = state.count - 1 }
-        | Reset -> init
+        | Install -> { model with Status = "Mod installed" }
+        | Uninstall -> { model with Status = "Mod uninstalled" }
     
-    let view (state: State) (dispatch) =
+    let view (model: Model) (dispatch) =
         DockPanel.create [
             DockPanel.children [
                 Button.create [
                     Button.dock Dock.Bottom
-                    Button.onClick (fun _ -> dispatch Reset)
-                    Button.content "reset"
+                    Button.onClick (fun _ -> dispatch Uninstall)
+                    Button.content "Uninstall"
                 ]                
                 Button.create [
                     Button.dock Dock.Bottom
-                    Button.onClick (fun _ -> dispatch Decrement)
-                    Button.content "-"
-                ]
-                Button.create [
-                    Button.dock Dock.Bottom
-                    Button.onClick (fun _ -> dispatch Increment)
-                    Button.content "+"
+                    Button.onClick (fun _ -> dispatch Install)
+                    Button.content "Install"
                 ]
                 TextBlock.create [
                     TextBlock.dock Dock.Top
-                    TextBlock.fontSize 48.0
+                    TextBlock.fontSize 32.0
                     TextBlock.verticalAlignment VerticalAlignment.Center
                     TextBlock.horizontalAlignment HorizontalAlignment.Center
-                    TextBlock.text (string state.count)
+                    TextBlock.text (string model.Status)
                 ]
             ]
         ]       
