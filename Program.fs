aa5d2561e29766b3c4d44f2edf0090a4dc101a12
modotte <runecroon@gmail.com>
2021-12-15 16:11:03 +0800

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,48 @@
+﻿namespace SAL
+
+open Elmish
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Input
+open Avalonia.FuncUI
+open Avalonia.FuncUI.Elmish
+open Avalonia.FuncUI.Components.Hosts
+
+type MainWindow() as this =
+    inherit HostWindow()
+    do
+        base.Title <- "SAL: SWAT4 Alternative Launcher"
+        base.Width <- 400.0
+        base.Height <- 400.0
+        
+        //this.VisualRoot.VisualRoot.Renderer.DrawFps <- true
+        //this.VisualRoot.VisualRoot.Renderer.DrawDirtyRects <- true
+
+
+        Elmish.Program.mkSimple (fun () -> Launcher.init) Launcher.update Launcher.view
+        |> Program.withHost this
+        |> Program.run
+
+        
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Load "avares://Avalonia.Themes.Default/DefaultTheme.xaml"
+        this.Styles.Load "avares://Avalonia.Themes.Default/Accents/BaseDark.xaml"
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            desktopLifetime.MainWindow <- MainWindow()
+        | _ -> ()
+
+module Program =
+
+    [<EntryPoint>]
+    let main(args: string[]) =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
