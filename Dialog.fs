374a3a1dd7c6ebf214aa4573bfa67241b51bbe3e
modotte <runecroon@gmail.com>
2021-12-23 03:56:47 +0800

    Simplify

--- Dialog.fs
+++ Dialog.fs
@@ -6,7 +6,7 @@ open Avalonia.Controls
 module Dialog =
     let getFolderDialog directory =
         let dialog = OpenFolderDialog()
-        dialog.Directory <- directory
         dialog.Title <- "Choose where to look up for SWAT4 directory"
+        dialog.Directory <- directory
         
         dialog
