c380fae06d759ed47b823539766ae9ffac1ed486
modotte <runecroon@gmail.com>
2021-12-26 21:56:54 +0800

    Simplify archive lock free

--- Archive.fs
+++ Archive.fs
@@ -9,6 +9,7 @@ open System.IO.Compression
 
 open SharpCompress.Common;
 open SharpCompress.Readers;
+open SharpCompress.Readers.Zip
 open SharpCompress.Readers.Rar
 
 open SharpCompress.Archives.SevenZip
@@ -16,36 +17,23 @@ open SharpCompress.Archives.SevenZip
 module Archive =
     let extractZipArchiveTo archive outputDir =
         use reader = ZipFile.Open(archive, ZipArchiveMode.Read)
-        try
-            try
-                reader.ExtractToDirectory(outputDir)
-            with
-            | :? IOException as exn ->
-                Logger.log.Error(exn.Message)
-        finally
-            reader.Dispose()
+
+        reader.ExtractToDirectory(outputDir)
 
     let extractRarArchiveTo archive outputDir =
         use fileReader = File.OpenRead(archive)
         use rarReader = RarReader.Open(fileReader)
-        try
-            while rarReader.MoveToNextEntry() do
-                let extractOptions = ExtractionOptions()
-                extractOptions.ExtractFullPath <- true
-                rarReader.WriteEntryToDirectory(outputDir, extractOptions)
-        finally
-            fileReader.Dispose()
-            rarReader.Dispose()
+
+        while rarReader.MoveToNextEntry() do
+            let extractOptions = ExtractionOptions()
+            extractOptions.ExtractFullPath <- true
+            rarReader.WriteEntryToDirectory(outputDir, extractOptions)
 
     let extractSevenZipArchiveTo archive outputDir =
         use fileReader = File.OpenRead(archive)
         use sevenZipReader = SevenZipArchive.Open(fileReader).ExtractAllEntries()
         
-        try
-            while sevenZipReader.MoveToNextEntry() do
-                let extractOptions = ExtractionOptions()
-                extractOptions.ExtractFullPath <- true
-                sevenZipReader.WriteEntryToDirectory(outputDir, extractOptions)
-        finally
-            fileReader.Dispose()
-            sevenZipReader.Dispose()
+        while sevenZipReader.MoveToNextEntry() do
+            let extractOptions = ExtractionOptions()
+            extractOptions.ExtractFullPath <- true
+            sevenZipReader.WriteEntryToDirectory(outputDir, extractOptions)
